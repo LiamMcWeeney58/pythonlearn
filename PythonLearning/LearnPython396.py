27291827a698445864fe301be802ec8aa4a8f0f0
Liam McWeeney <94412396+LiamMcWeeney58@users.noreply.github.com>
2022-01-27 17:26:20 -0500

    1st Save

---
+++ PythonLearning/LearnPython396.py
@@ -0,0 +1,10 @@
+# test comment
+print ('hello world')
+2+2
+50+2*4 #order of operations is used 
+5*2 #One star symbol '*' is used for multiplication (5*2=10)
+5**2 #Two stars '**' is used to calculate powers (ex: 5**2 = 25 because 5x5=25)
+length = 100 #example of how to assign a variable 
+height = 9*3 #note for variables: they use order of operations so the order the variables in equation are assigned could change answer
+5.25*10**3 #Floating point math is supported (5.25*10**3= 525.0)
+#the underscore symbol '_' is used to define the last number used. For example: 5.25*10**3= 525.0, the 525.0 now is the '_' so if you were to do _/2 you would get 262.5
