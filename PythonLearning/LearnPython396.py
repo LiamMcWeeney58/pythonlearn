6c8f64c7c11d56a755afd938b5c7f56d6386ecea
Liam McWeeney <94412396+LiamMcWeeney58@users.noreply.github.com>
2022-01-27 22:36:46 -0500

    1/27/2022 10:36pm
    First Backup

--- PythonLearning/LearnPython396.py
+++ PythonLearning/LearnPython396.py
@@ -8,3 +8,8 @@ length = 100 #example of how to assign a variable
 height = 9*3 #note for variables: they use order of operations so the order the variables in equation are assigned could change answer
 5.25*10**3 #Floating point math is supported (5.25*10**3= 525.0)
 #the underscore symbol '_' is used to define the last number used. For example: 5.25*10**3= 525.0, the 525.0 now is the '_' so if you were to do _/2 you would get 262.5
+tax = 17.5/47
+price = 58
+price * tax
+price + _ #use of previous number ex: the '_' will be 21.59574 as that is the previous number
+round(_,2) #rounding rounds a decimal to a certain amount of decimal places (of your choice!) How it works: round(x,y)  x= the decimal you want to round  y= how many places you want to round
