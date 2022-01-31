ca7fc192a23eb7c2c5d610db30273dad304986cf
Liam McWeeney <94412396+LiamMcWeeney58@users.noreply.github.com>
2022-01-31 18:35:37 -0500

    Update LearnPython396.py

--- PythonLearning/LearnPython396.py
+++ PythonLearning/LearnPython396.py
@@ -17,4 +17,11 @@ round(_,2) #rounding rounds a decimal to a certain amount of decimal places (of 
 'won\'t you be my neighbor' #for words with the "'" symbol you will need to put a "\" before the "'" this will make the output have double quotes '""' 
 "won't you be my neighbor" #same output as above however you don't need the '\' symbol as double quotes '""' are used
 '"Won\'t you be my neighbor?" said Mr.Rodgers'#A quote inside a quote, this is the breaking point. The output of this line is '"Won\'t you be my neighbor?" said Mr.Rodgers' THIS IS NOT WHAT YOU WANT!
-print
+print('"Won\'t you be my neighbor?" said Mr.Rodgers') #This is what you want, the 'print()' function gets you a working output that you want 
+qwerty = '1st\n2nd' #the 'n\' creates a new line for the text after it. Making h = print('text\nhere') will result in the output being 'none'
+print(qwerty)
+print('william\nathan\james') #this output cuts off the letter n
+print(r'william\nathan\james') #the 'r' before the printed text 
+print("""L1
+L2
+L3""")  #triple quotation marks can expand accross multiple lines
