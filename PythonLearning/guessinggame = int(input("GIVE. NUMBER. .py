56cb7ceec8a02bd320ee2d2bc2662267dd1bd575
Liam McWeeney <94412396+LiamMcWeeney58@users.noreply.github.com>
2022-02-08 19:34:07 -0500

    Number Guess
    WIP Number Guessing Game! Still in the works as I learn python

---
+++ PythonLearning/guessinggame = int(input("GIVE. NUMBER. .py
@@ -0,0 +1,9 @@
+guessinggame = int(input("GIVE. NUMBER. NOW:"))
+#int gets rid of decimal 
+if guessinggame = 58:
+    guessinggame = 58
+    print('Nice')
+elif guessinggame < 58
+    print('FOOL! Try again.')
+elif guessinggame > 58
+    print('try again')
