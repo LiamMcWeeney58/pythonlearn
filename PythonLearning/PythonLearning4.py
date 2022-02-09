a52ff923cbc98cecee0eb7fdb4440f03eae01259
Liam McWeeney <94412396+LiamMcWeeney58@users.noreply.github.com>
2022-02-08 19:32:52 -0500

    Python Learning 4

---
+++ PythonLearning/PythonLearning4.py
@@ -0,0 +1,13 @@
+#testing while() functon
+#a, b = 0, 1
+#while a < 1000:
+    #print(a, end=',')
+    #a, b = b, a+b
+
+
+
+River = 12272005
+if River > 8182005: #'if' certain input then action
+    print('I like your art dude! It\'s really good!')
+    River-8182005\
+#Input checks for input
